242cf02d9a1e88c120a636a9f2e312e6b5d0857a
farhad <farhad.biyouk1999@gmail.com>
2025-02-08 14:15:27 -0500

    finished post and subscription section.

--- core/authentication/urls.py
+++ core/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+
 urlpatterns = [
 	path('jwt/', include(([
 		path('login/', TokenObtainPairView.as_view(), name='login'),
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
 from core.common.models import BaseModel
+from core.users.models import BaseUser
 
 
-class Product(BaseModel):
-	name = models.CharField(max_length=255)
+
+class Post(BaseModel):
+	slug = models.SlugField(
+		primary_key=True,
+		max_length=200
+	)
+	
+	title = models.CharField(max_length=150, unique=True)
+	content = models.TextField()
+	author = models.ForeignKey(BaseUser, on_delete=models.SET_NULL, null=True)
+	
+	def __str__(self):
+		return self.slug
+	
+class Subscription(models.Model):
+	subscriber = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name='subs')
+	target = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name='targets')
+	
+	class Meta:
+		unique_together =('subscriber', 'target')
+	
+	def clean(self):
+		if self.subscriber == self.target:
+			raise ValidationError({'subscriber':('subscriber cannot to equal to target',) })
+	
+	def __str__(self):
+		return f'{self.subscriber.email} - {self.target.email}'
